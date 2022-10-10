9e1dd452c9528addc35110ee55218d00c540100f
Abhishek Jha <abhishek.jha.uk@gmail.com>
2022-10-10 14:51:19 +0100

    README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Python Intro
+## Why Python
+### Python Use Case
+#### Python set up with pycharm
+##### Python Variable
+
+```commandline
+print()
+```
---
+++ env_test.py
@@ -0,0 +1,23 @@
+#testing the env with print statement
+print("Hello World!")
+
+#to store user given data
+
+# first_name and last_name
+# display the names in the line
+# user DOB
+# course name
+# UK_resident
+# add to git hub
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Your name is " + first_name + " " + last_name)
+
+dob = input("Enter you Date of Birth: ")
+course_name = input("Enrolled Course Name: ")
+resident = input("Are you a UK resident: ")
+print(dob)
+print(course_name)
+print(resident)
